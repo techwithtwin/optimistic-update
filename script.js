9576ee0d05e59978d10d402d7af660658362ed7d
TechWithTwin <realwebtechguru@gmail.com>
2025-09-08 16:57:33 +0300

    adding javascript logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const posts = document.querySelector(".posts");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const title = data.get("title");
+  const desc = data.get("description");
+
+  if (!title || !desc) {
+    alert("Invalid data");
+    return;
+  }
+}
