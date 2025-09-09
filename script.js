ce2a25dbb3bf28c4be1b62f7194a23b55967501f
TechWithTwin <realwebtechguru@gmail.com>
2025-09-10 00:23:27 +0300

    adding js logic

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const posts = document.querySelector(".posts");
 const form = document.querySelector("form");
+const baseUrl = "https://jsonplaceholder.typicode.com";
+const loader = document.querySelector(".loader");
 
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
   const data = new FormData(e.target);
   const title = data.get("title");
@@ -12,5 +14,51 @@ function handleSubmit(e) {
   if (!title || !desc) {
     alert("Invalid data");
     return;
+  }
+
+  loader.style.display = "block";
+  const resData = await savePost(title, desc);
+  loader.style.display = "none";
+
+  if (resData) {
+    const newPost = `
+    <div class="post">
+        <h3>
+          ${resData.title}
+        </h3>
+        <p>
+         ${resData.body}
+        </p>
+      </div>
+    `;
+    const newHtml = newPost + posts.innerHTML;
+    posts.innerHTML = newHtml;
+  }
+}
+
+async function savePost(title, desc) {
+  try {
+    const res = await fetch(baseUrl + "/posts", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        title,
+        body: desc,
+        userId: 1,
+      }),
+    });
+
+    if (!res.ok) throw new Error("Failed to fetch");
+
+    const data = await res.json();
+
+    console.log(title, desc);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
 }
